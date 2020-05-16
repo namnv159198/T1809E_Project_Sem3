75e2ee168182285c1462ae98a2528d1c0fcc08ed
TimmocEdoc <fraggler131@gmail.com>
2020-05-16 13:28:49 +0700

    Create category

---
+++ T1809E_Project_Sem3/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace T1809E_Project_Sem3.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Display(Name = "Status")]
+        public StatusEnum Status { get; set; }
+        public enum StatusEnum
+        {
+            Active = 0,
+            Deactive = 1,
+            Delete = -1
+        }
+    }
+}
