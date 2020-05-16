c7203a47baba1433742b4333dc8cf34952242922
TimmocEdoc <fraggler131@gmail.com>
2020-05-16 11:43:38 +0700

    Create Category

---
+++ T1809E_Project_Sem3/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace T1809E_Project_Sem3.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public EnumStatus Status { get; set; }
+        public enum EnumStatus
+        {
+            Active = 0,
+            Deactive = 1,
+            Deleted = -1
+        }
+    }
+}
