db03f513dbe77b760df285a147b993b058809bfb
tandat01 <dat01101999@gmail.com>
2020-05-19 18:38:59 +0700

    edit seed Category

--- T1809E_Project_Sem3/Migrations/Configuration.cs
+++ T1809E_Project_Sem3/Migrations/Configuration.cs
@@ -17,9 +17,10 @@
         {
             //  This method will be called after migrating to the latest version.
             context.Categories.AddOrUpdate(x => x.Id,
-                new Category() { Id = 1, Name = "Đồng hồ Men", Status = Category.StatusEnum.Active },
-                new Category() { Id = 2, Name = "Đồng hồ Lady", Status = Category.StatusEnum.Active },
-                new Category() { Id = 3, Name = "Đồng hồ Kid", Status = Category.StatusEnum.Active }
+                new Category() { Id = 1, Name = "Men", Status = Category.StatusEnum.Active },
+                new Category() { Id = 2, Name = "Lady", Status = Category.StatusEnum.Active },
+                new Category() { Id = 3, Name = "Boy Kid", Status = Category.StatusEnum.Active },
+                new Category() { Id = 3, Name = "Girl Kid", Status = Category.StatusEnum.Active }
             );
            
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
