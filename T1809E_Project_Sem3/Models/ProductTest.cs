924593135e423d0b1e5f4279abca767a72d6187d
nam159198 <namkun159198@gmail.com>
2020-05-20 15:33:22 +0700

    Test product

---
+++ T1809E_Project_Sem3/Models/ProductTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T1809E_Project_Sem3.Models
+{
+    public class ProductTest
+    {
+        public int ID { get; set; }
+    }
+}
