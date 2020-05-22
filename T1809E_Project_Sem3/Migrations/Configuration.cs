9255bb03767c353937609d914b143e6e3733d842
nam159198 <namkun159198@gmail.com>
2020-05-22 21:29:58 +0700

    Edit form product

--- T1809E_Project_Sem3/Migrations/Configuration.cs
+++ T1809E_Project_Sem3/Migrations/Configuration.cs
@@ -19,28 +19,28 @@
         {
 
             //  This method will be called after migrating to the latest version.
-            //context.Categories.AddOrUpdate(x => x.Id,
-            //    new Category() { Id = 0, Name = "Men", Status = Category.StatusEnum.Active },
-            //    new Category() { Id = 1, Name = "Lady", Status = Category.StatusEnum.Active },
-            //    new Category() { Id = 2, Name = "Boy Kid", Status = Category.StatusEnum.Active },
-            //    new Category() { Id = 3, Name = "Girl Kid", Status = Category.StatusEnum.Active }
-            //);
-            //var adminId = "Admin";
-            //var adminRoleId = "Admin";
-            //if (!context.Roles.Any(r => r.Id == adminRoleId))
-            //{
-            //    var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            //    var role = new IdentityRole { Id = adminRoleId, Name = "Admin" };
-            //    roleManager.Create(role);
-            //}
-            //var adminId2 = "Employee";
-            //var adminRoleId2 = "Employee";
-            //if (!context.Roles.Any(r => r.Id == adminRoleId2))
-            //{
-            //    var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            //    var role = new IdentityRole { Id = adminRoleId2, Name = "Employee" };
-            //    roleManager.Create(role);
-            //}
+            context.Categories.AddOrUpdate(x => x.Id,
+                new Category() { Id = 0, Name = "Men", Status = Category.StatusEnum.Active },
+                new Category() { Id = 1, Name = "Lady", Status = Category.StatusEnum.Active },
+                new Category() { Id = 2, Name = "Boy Kid", Status = Category.StatusEnum.Active },
+                new Category() { Id = 3, Name = "Girl Kid", Status = Category.StatusEnum.Active }
+            );
+            var adminId = "Admin";
+            var adminRoleId = "Admin";
+            if (!context.Roles.Any(r => r.Id == adminRoleId))
+            {
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+                var role = new IdentityRole { Id = adminRoleId, Name = "Admin" };
+                roleManager.Create(role);
+            }
+            var adminId2 = "Employee";
+            var adminRoleId2 = "Employee";
+            if (!context.Roles.Any(r => r.Id == adminRoleId2))
+            {
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+                var role = new IdentityRole { Id = adminRoleId2, Name = "Employee" };
+                roleManager.Create(role);
+            }
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
