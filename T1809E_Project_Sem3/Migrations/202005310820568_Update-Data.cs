a3d8af5ec9f3e0fe36ada67f87137972c192d879
TimmocEdoc <fraggler131@gmail.com>
2020-05-31 15:24:56 +0700

    fix branch

---
+++ T1809E_Project_Sem3/Migrations/202005310820568_Update-Data.cs
@@ -0,0 +1,172 @@
+namespace T1809E_Project_Sem3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateData : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Status = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        Status = c.Int(nullable: false),
+                        Description = c.String(nullable: false),
+                        Thumbnails = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Discount = c.Int(nullable: false),
+                        CreateAt = c.DateTime(),
+                        CategoryID = c.Int(nullable: false),
+                        CreateById = c.String(maxLength: 128),
+                        UpdateById = c.String(maxLength: 128),
+                        DeleteById = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryID, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.CreateById)
+                .ForeignKey("dbo.AspNetUsers", t => t.DeleteById)
+                .ForeignKey("dbo.AspNetUsers", t => t.UpdateById)
+                .Index(t => t.CategoryID)
+                .Index(t => t.CreateById)
+                .Index(t => t.UpdateById)
+                .Index(t => t.DeleteById);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Address = c.String(),
+                        Birthday = c.DateTime(),
+                        Gender = c.Int(nullable: false),
+                        Status = c.Int(nullable: false),
+                        CreateAt = c.DateTime(nullable: false),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.UserCustomerViewModels",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        UserName = c.String(nullable: false, maxLength: 50),
+                        Address = c.String(nullable: false, maxLength: 100),
+                        Email = c.String(nullable: false),
+                        Phonenumber = c.String(nullable: false, maxLength: 10),
+                        CreatedAt = c.DateTime(),
+                        Status = c.Int(nullable: false),
+                        Gender = c.Int(nullable: false),
+                        Customer_Type = c.Int(nullable: false),
+                        Total_Money_Purchased = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Total_Quantity_Purchased = c.Int(nullable: false),
+                        Total_Purchased = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Products", "UpdateById", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Products", "DeleteById", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Products", "CreateById", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Products", "CategoryID", "dbo.Categories");
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.Products", new[] { "DeleteById" });
+            DropIndex("dbo.Products", new[] { "UpdateById" });
+            DropIndex("dbo.Products", new[] { "CreateById" });
+            DropIndex("dbo.Products", new[] { "CategoryID" });
+            DropTable("dbo.UserCustomerViewModels");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
