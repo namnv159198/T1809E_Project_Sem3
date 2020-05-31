39e82549b0da3566dcfa00bc511bf90cd6b1cd38
TimmocEdoc <fraggler131@gmail.com>
2020-05-31 13:51:46 +0700

    fix branch

---
+++ T1809E_Project_Sem3/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T1809E_Project_Sem3.Models
+{
+    public class Customer
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public EnumGender Gender { get; set; }
+        public enum EnumGender 
+        {
+            Male = 0,
+            Female = 1,
+            Other = 2
+        }
+        public DateTime CreateAt { get; set; }
+        public int Total_Quantity_Purchased { get; set; }
+        public int Total_Money_Purchased { get; set; }
+    }
+}
