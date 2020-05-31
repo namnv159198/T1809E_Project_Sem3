466b0d763133603a32a9f31f4914ba493933e22f
TimmocEdoc <fraggler131@gmail.com>
2020-05-31 14:36:26 +0700

    fix branch

--- T1809E_Project_Sem3/Migrations/202005310730049_Update-Data.Designer.cs
+++ T1809E_Project_Sem3/Migrations/202005310730049_Update-Data.Designer.cs
@@ -13,7 +13,7 @@ namespace T1809E_Project_Sem3.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "202005220325582_Update-Data"; }
+            get { return "202005310730049_Update-Data"; }
         }
         
         string IMigrationMetadata.Source
