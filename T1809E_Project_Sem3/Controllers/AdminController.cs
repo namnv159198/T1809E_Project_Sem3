14dc9de6f03be6640bb838d462dd283dbdba071e
LinhRua <44184455+LinhRua@users.noreply.github.com>
2020-06-05 16:00:11 +0700

    xay dung trang chu quan tri

--- T1809E_Project_Sem3/Controllers/AdminController.cs
+++ T1809E_Project_Sem3/Controllers/AdminController.cs
@@ -43,6 +43,10 @@ namespace T1809E_Project_Sem3.Controllers
         {
             return View();
         }
-
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+       
     }
 }
