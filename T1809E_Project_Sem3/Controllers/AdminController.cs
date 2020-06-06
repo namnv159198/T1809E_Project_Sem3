69a43d80d63e551349be8999ce6e75678f8699a4
LinhRua <44184455+LinhRua@users.noreply.github.com>
2020-06-06 18:35:47 +0700

    them totalproduct

--- T1809E_Project_Sem3/Controllers/AdminController.cs
+++ T1809E_Project_Sem3/Controllers/AdminController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using T1809E_Project_Sem3.Models;
 
 namespace T1809E_Project_Sem3.Controllers
 {
     public class AdminController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Admin
         public ActionResult Index()
         {
+            
             return View();
         }
         
@@ -45,6 +49,9 @@ namespace T1809E_Project_Sem3.Controllers
         }
         public ActionResult Homepage()
         {
+            ViewBag.totalProduct = db.Products.Count();
+            var orders = db.Orders.Where(c => c.CreatedAt == DateTime.Now);
+            ViewBag.totalPrice = orders.Sum(o => o.TotalPrice);
             return View();
         }
        
