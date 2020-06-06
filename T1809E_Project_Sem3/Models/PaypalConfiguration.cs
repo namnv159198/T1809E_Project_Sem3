0acd12dc5e25d9dc10b7d915f4a7688e5b2d3959
AnhNT <anhntd00558@fpt.edu.vn>
2020-06-06 17:05:41 +0700

    model and configuration PayPal

---
+++ T1809E_Project_Sem3/Models/PaypalConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using PayPal.Api;
+namespace T1809E_Project_Sem3.Models
+{
+    public class PaypalConfiguration
+    {
+        public readonly static string ClientId;
+        public readonly static string ClientSecret;
+
+        static PaypalConfiguration()
+        {
+            var config = GetConfig();
+            ClientId = config["ClientId"];
+            ClientSecret = config["ClientSecret"];
+        }
+        public static Dictionary<string , string> GetConfig()
+        {
+            return PayPal.Api.ConfigManager.Instance.GetProperties();
+        }
+
+        private static string GetAccessToken()
+        {
+            string accessToken = new OAuthTokenCredential(ClientId, ClientSecret, GetConfig()).GetAccessToken();
+            return accessToken;
+        }
+        public static APIContext GetAPIContext()
+        {
+            APIContext apiContext = new APIContext(GetAccessToken());
+            apiContext.Config = GetConfig();
+            return apiContext;
+        }
+    }
+}
---
+++ T1809E_Project_Sem3/Models/PaypalLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace T1809E_Project_Sem3.Models
+{
+    public class PaypalLogger
+    {
+        public static string LogDirecttoryPath = Environment.CurrentDirectory;
+
+        public static void Log (String messages)
+        {
+            try
+            {
+                StreamWriter strw = new StreamWriter(LogDirecttoryPath + "\\PaypalError.log", true);
+                strw.WriteLine("{0}--->{1}", DateTime.Now.ToString("MM/dd/yyy HH:mm:ss"), messages);
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
